c31ca3384f93280282a91b61f4a79f20f6d9d07d
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 10:10:54 -0500

    initialized color props and passed three of them into picker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 // Write your Color component here
+import { useState } from "react";
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+  
+  const Color = (props) => {
+    return <div className={props.color}></div>
+  }
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color= "black" />
+        <Color color= "violet" />
+        <Color color= "red" />
+      </div>
     </div>
   );
 };
